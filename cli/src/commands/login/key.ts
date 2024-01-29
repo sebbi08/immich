7dd82db341ec5b1989421ce27f8741655609cf5f
bo0tzz <git@bo0tzz.me>
2024-01-29 20:30:37 +0000

    fix(cli): Throw error on missing instanceUrl

--- cli/src/commands/login/key.ts
+++ cli/src/commands/login/key.ts
@@ -2,6 +2,10 @@ import { BaseCommand } from '../../cli/base-command';
 
 export class LoginKey extends BaseCommand {
   public async run(instanceUrl: string, apiKey: string): Promise<void> {
+    if (!instanceUrl) {
+      throw new Error("instanceUrl is required.")
+    }
+    
     console.log('Executing API key auth flow...');
 
     await this.sessionService.keyLogin(instanceUrl, apiKey);
